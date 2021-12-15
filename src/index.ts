bf596864402cc75a527d9979d2e20702b904c05f
shoom3301 <shoom3301@gmail.com>
2021-12-15 11:47:46 +0600

    Hash structure and sign data hash

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,39 @@
-import {TypedDataUtils, TypedMessage} from "eth-sig-util";
-import {EIP712TypedData, MessageTypes} from "./models";
+import {EIP712TypedData, MessageTypes} from './models';
+import {TypedDataUtils, TypedMessage} from 'eth-sig-util';
 
-const WORLD = 'world';
-
-export function hello(world: string = WORLD): string {
-    return `Hello ${world}! `;
-}
-
-export type LimitOrderHash = string;
-
-const data:EIP712TypedData = {
-    message: {
-        operation:"0",
-        nonce:"0",
-        token:{
-        name:"Ethereum",
-            decimals:18,
-            symbol:"ETH",
-            logoUri:"https://gnosis-safe.io/app/static/media/token_eth.bc98bd46.svg",
-            address:"main"
-        },
-        recipient:"0x60AF6197d08dEa71A22581f13B72018B8C2671A7",
-        type:"transferFunds",
-        data:"0x",
-        to:"0x60AF6197d08dEa71A22581f13B72018B8C2671A7",
-        amount:"10000000000000000",
-        value:"10000000000000000"
+// I don't think this part is correct based on the docs: https://github.com/gnosis/zodiac-module-reality#domain
+const typedData: EIP712TypedData = {
+    primaryType: 'Transaction',
+    types: {
+        Transaction: [
+            { type: 'address', name: 'to' },
+            { type: 'uint256', name: 'value' },
+            { type: 'bytes', name: 'data' },
+            { type: 'uint8', name: 'operation' },
+            { type: 'uint256', name: 'nonce' }
+        ]
     },
-    primaryType: 'EIP712Domain',
-    domain: { // I don't think this part is correct based on the docs: https://github.com/gnosis/zodiac-module-reality#domain
+    domain: {
         chainId: 1,
         verifyingContract: '0x1234'
     },
-    types: {
-        Transaction: [
-            { type: "address", name: "to" },
-            { type: "uint256", name: "value" },
-            { type: "bytes", name: "data" },
-            { type: "uint8", name: "operation" },
-            { type: "uint256", name: "nonce" }
-        ]
+    message: {
+        to:'0x60AF6197d08dEa71A22581f13B72018B8C2671A7',
+        value:'10000000000000000',
+        data:'0x',
+        operation:'0',
+        nonce:'0',
     },
 }
 
-function buildLimitOrderHash(orderTypedData: EIP712TypedData): LimitOrderHash {
-    const message = orderTypedData as TypedMessage<MessageTypes>;
+const hashedStructure = TypedDataUtils.hashStruct(
+    typedData.primaryType,
+    typedData.message,
+    typedData.types,
+    true
+).toString('hex');
 
-    return '0x' + TypedDataUtils.sign(message).toString('hex');
-}
+const signedDataHash = '0x' + TypedDataUtils.sign(typedData as TypedMessage<MessageTypes>).toString('hex');
 
-console.log("Limit order: ")
-console.log(data);
-
-const limitOrderHash = buildLimitOrderHash(data)
-
-console.log("Limit order hash: ")
-console.log(limitOrderHash);
+console.log('hashedStructure: ', hashedStructure);
+console.log('signedDataHash: ', signedDataHash);
